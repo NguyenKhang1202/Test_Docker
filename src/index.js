fd603585ed8338d9a6c23f2adeb8785f076b8059
NguyenKhang1202 <khang.nv182000@gmail.com>
2023-08-06 16:08:21 +0700

    chore: test jenkins

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 // App
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World jenkins");
 });
 
 app.listen(PORT, () => {
